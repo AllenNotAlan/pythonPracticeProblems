fff52f680f29e186c4524ba18e36eefe8d897f48
Allen Loyola <allenloyola23@gmail.com>
2020-05-28 21:23:06 +0100

    hangman game almost* complete. In guessLetters.py

--- Beginner/guessLetters.py
+++ Beginner/guessLetters.py
@@ -1,43 +1,64 @@
-word = "evaporate"
+from pickword import pickWord
+
+word = pickWord("sowpods.txt").lower()
 wordArray = []
 displayWordArray = []
-usedLetters = []
+usedLetters = set()
+score = 6
 
-for i in word:
-    wordArray.append(i)
-print(wordArray)
+def initialiseArrays(word, wordArray, displayWordArray):
+    for i in word:
+        wordArray.append(i)
 
-for i in range(len(word)):
-    displayWordArray.append("_")
-print(displayWordArray)
+    for i in range(len(word)):
+        displayWordArray.append("_")
 
-def checkLetterUsed(userInput):
-    if userInput in usedLetters:
-        prompt = "You've already used that letter."
-    return prompt
+def checkLetterUsed(userInput,lettersUsedSet):
+    if userInput in lettersUsedSet:
+        print("You've already used that letter")
 
-def usedLettersToSet(usedLetters):
-    print(set(usedLetters))
-
-gameOn = True
-while gameOn:
-    userGuess = str(input("Enter a letter: "))
-    usedLetters.append(userGuess)
-    if userGuess in wordArray:
-        while userGuess in wordArray:
+def checkLetterInWordArray(userInput, wordArray):
+    if userInput in wordArray:
+        while userInput in wordArray:
             for i in range(len(wordArray)):
-                if userGuess == wordArray[i]:
-                    displayWordArray[i] = userGuess
+                if userInput == wordArray[i]:
+                    displayWordArray[i] = userInput
             break
     else:
-        usedLetters.append(userGuess)
         print("Letter not in word")
-    
-    print(displayWordArray)
-    print(usedLetters)
+        global score
+        score = score - 1
 
-    if displayWordArray == wordArray:
-        print("You guessed the word!")
-        gameOn = False
 
-usedLettersToSet(usedLetters)
+def printInfo(displayWordArray, lettersUsedSet):
+    print(displayWordArray,"\n")
+    print("Letters you've used:")
+    print(lettersUsedSet)
+    print("")
+
+def printIntro():
+    print("Welcome to hangman!\nWhat is this word:")
+
+def main():
+    printIntro()
+    initialiseArrays(word, wordArray, displayWordArray)
+    gameOn = True
+    while gameOn:
+        printInfo(displayWordArray, usedLetters)
+        userGuess = str(input("Enter a letter: "))
+        checkLetterUsed(userGuess, usedLetters)
+        usedLetters.add(userGuess)
+        checkLetterInWordArray(userGuess, wordArray)
+        print(score)
+
+        if score < 1:
+            print("Game over")
+            break
+
+        if displayWordArray == wordArray:
+            print("You guessed the word!")
+            print("The word was: %s" % word.upper())
+            gameOn = False
+
+if __name__ == "__main__":
+    main()
--- Beginner/pickword.py
+++ Beginner/pickword.py
@@ -1,15 +1,16 @@
 from random import randint
 
-listOfString = []
-with open('sowpods.txt', 'r') as f:
-    line = f.readline()
+def pickWord(fileName):
+    listOfString = []
+    with open(fileName, 'r') as f:
+        line = f.readline()
 
-    while line:
-        line = f.readline().strip()
-        listOfString.append(line)
-    
-randomNum = randint(0,len(listOfString))
-randomWord = listOfString[randomNum]
+        while line:
+            line = f.readline().strip()
+            listOfString.append(line)
+        
+    randomNum = randint(0,len(listOfString))
+    randomWord = listOfString[randomNum]
 
-print("Picking random word...")
-print(randomWord)
+    print("Picking random word...")
+    return randomWord
